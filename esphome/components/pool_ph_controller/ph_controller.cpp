a020c0d46565ccc907e600e83bfcfce69ae83c0e
Fabian <fabian.seither@gmail.com>
2026-08-22 22:13:02 +0200

    next fix

--- esphome/components/pool_ph_controller/ph_controller.cpp
+++ esphome/components/pool_ph_controller/ph_controller.cpp
@@ -2,7 +2,7 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/lib/water_chemistry.h"
+#include "../pool_lib/water_chemistry.h"
 
 #include <algorithm>
 #include <cmath>
