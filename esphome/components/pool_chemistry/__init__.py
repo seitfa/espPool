fcb2060d66a471fb01746f9424b17221fbc1be47
Fabian <fabian.seither@gmail.com>
2026-08-26 21:41:14 +0200

    Re-Add pool_chemitry as component

---
+++ esphome/components/pool_chemistry/__init__.py
@@ -0,0 +1,5 @@
+"""Pool chemistry helper library for ESPHome external components."""
+
+# This component is a plain C++ library component. No code generation is required
+# for the helper API itself. It is intended to be consumed by other components via
+# an include such as `#include "pool_chemistry.h"`.
--- esphome/components/pool_ph_controller/ph_controller.h
+++ esphome/components/pool_ph_controller/ph_controller.h
@@ -5,7 +5,7 @@
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 
-#include <pool_chemistry.h>
+#include "pool_chemistry.h"
 
 namespace esphome {
 namespace pool_controller {
